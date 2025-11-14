a496474e1c89c05a75d430b252fc9acb0bff8cd5
2concrete <danielpeace654@gmail.com>
2025-11-14 23:59:25 +0800

    another posthog fix

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,9 +2,5 @@ import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  person_profiles: "identified_only",
-  // Add these cookie configuration options:
-  persistence: "localStorage+cookie", // Use localStorage as fallback
-  cross_subdomain_cookie: false,
-  secure_cookie: false, // Set to false for localhost
+  defaults: "2025-05-24",
 });
