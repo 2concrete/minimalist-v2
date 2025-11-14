762d57bfd5154c8f65d884b7abfea08aa655b7cc
2concrete <danielpeace654@gmail.com>
2025-11-15 00:10:22 +0800

    this actually just wont work

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,7 +3,6 @@ import posthog from "posthog-js";
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   defaults: "2025-05-24",
-  cross_subdomain_cookie: true,
   persistence: "localStorage+cookie",
-  capture_pageview: true, // Explicitly enable
+  capture_pageview: true,
 });
