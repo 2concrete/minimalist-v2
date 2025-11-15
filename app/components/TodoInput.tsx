ee73689aafd8e9772c8b549bf0ffe28494a7376a
2concrete <danielpeace654@gmail.com>
2025-11-15 12:28:15 +0800

    more fixing

--- app/components/TodoInput.tsx
+++ app/components/TodoInput.tsx
@@ -6,6 +6,7 @@ import { motion } from "motion/react";
 import { useState, useContext } from "react";
 import { HiOutlineChevronRight } from "react-icons/hi2";
 import { TodoContext } from "../hooks/TodoContext";
+import posthog from "posthog-js";
 
 type TodoInputProps = {
   isAuthenticated: boolean;
@@ -28,6 +29,8 @@ const TodoInput = ({ isAuthenticated }: TodoInputProps) => {
     } else if (Context?.addTodo) {
       Context.addTodo(title);
     }
+
+    posthog.capture("submit");
 
     setTitle("");
   };
